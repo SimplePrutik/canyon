e144753d782894a7f2645ff4f125aaaa8b5dfd6b
SimplePrutik <dimodaktil@yandex.ru>
2016-04-28 14:18:50 +0300

    initial commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+    
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+    bool SensorForward()
+    {
+        return true;
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.position += transform.forward * 5 * Time.deltaTime;
+        //transform.Rotate(new Vector3(0, 50, 0) * Time.deltaTime);
+        
+    }
+}
---
+++ Assets/Scripts/PathGenerator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+public class MazeGen
+{
+    public List<List<bool>> maze;
+
+    public void Generate(int size)
+    {
+        List<bool> f;
+    }
+}

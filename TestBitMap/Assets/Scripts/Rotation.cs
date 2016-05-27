2fcdcc3f04c0f61346c96b4552a9579bbb7a619f
SimplePrutik <dimodaktil@yandex.ru>
2016-05-28 00:35:39 +0300

    added code for expert system

---
+++ TestBitMap/Assets/Scripts/Rotation.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotation : MonoBehaviour {
+
+    [Header("Zero")]
+    public float z1;
+    public float z2;
+    public float z3;
+    public float z4;
+
+    [Header("One")]
+    public float o1;
+    public float o2;
+    public float o3;
+    public float o4;
+
+    [Header("Two")]
+    public float t1;
+    public float t2;
+    public float t3;
+    public float t4;
+}

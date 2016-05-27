7aa4263807af5d236bc4ad803d7736b557114340
SimplePrutik <dimodaktil@yandex.ru>
2016-05-28 00:30:05 +0300

    CAR INSTATNTIEASDA

--- TestBitMap/Assets/Scripts/MainGameScript.cs
+++ TestBitMap/Assets/Scripts/MainGameScript.cs
@@ -4,6 +4,8 @@ using System.Collections;
 
 public class MainGameScript : MonoBehaviour {
 
+    public UnityEngine.UI.InputField SX;
+    public UnityEngine.UI.InputField SY;
     public GameObject Block;
     public GameObject QuitBlock;
     public UnityEngine.UI.Image imgg;
@@ -113,10 +115,7 @@ public class MainGameScript : MonoBehaviour {
     // Use this for initialization
     void Start () {
         GenMap();
-		ShowDots coordinates = GetComponent<ShowDots>();
-		int x = System.Convert.ToInt32(coordinates.SX.text);
-		int y = System.Convert.ToInt32 (coordinates.SY.text);
-		Instantiate (Resources.Load ("Car"), new Vector3 (x, y, 0), Quaternion.identity);
+		Instantiate (Resources.Load ("Car"), new Vector3 (System.Convert.ToInt32(SX.text), System.Convert.ToInt32(SY.text), 0), Quaternion.identity);
 	}
 	
 	// Update is called once per frame
